1be92b6a81f4ea60aea62d3573aa24fc1ba35850
wenyue gu <wg74@duke.edu>
2020-09-10 17:58:25 -0700

    test tidybio for five classes

--- R/test_tidybiology.R
+++ R/test_tidybiology.R
@@ -8,8 +8,9 @@ library(tidyverse)
 library(lubridate)
 library(proteins)
 
-#class 1
 data(proteins)
+
+#class 1
 prepare_data(bespoke_dataframe = proteins,
              test = TRUE)
 
@@ -26,15 +27,15 @@ prepare_metadata(bespoke_dataframe = proteins,
 prepare_yaml(title = "Tidybiology",
              subtitle = "Intro to Data Science using R",
              author = "Matthew Hirschey & Akshay Bareja",
-             date = today(),
+             date = as.Date("2020-11-04"),
              test = TRUE)
 
-make_lecture(lecture_num = c(10, 11, 20),
+make_lecture(lecture_num = c(10, 11, 20,21,22,23,30,90,91.92),
              name = "class1",
              test = TRUE)
 
-# launch_lecture(name = "class1",
-#                 test = TRUE)
+launch_lecture(name = "class1",
+               test = TRUE)
 
 #class 2
 prepare_data(bespoke_dataframe = proteins,
@@ -53,22 +54,120 @@ prepare_metadata(bespoke_dataframe = proteins,
 prepare_yaml(title = "Tidybiology",
              subtitle = "Intro to Data Science using R",
              author = "Matthew Hirschey & Akshay Bareja",
-             date = today(),
+             date = as.Date("2020-11-06"),
              test = TRUE)
 
-make_lecture(lecture_num = c(10, 11, 20),
+make_lecture(lecture_num = c(31,32,33,34,35,36,37,38,39),
              name = "class2",
              test = TRUE)
 
-# launch_lecture(name = "class2",
-#                test = TRUE)
+launch_lecture(name = "class2",
+              test = TRUE)
 
 #class 3
+prepare_data(bespoke_dataframe = proteins,
+             test = TRUE)
+
+prepare_metadata(bespoke_dataframe = proteins,
+                 dataframe_name = "proteins",
+                 dataframe_title = "proteins",
+                 dataframe_source = "Uniprot",
+                 dataframe_desc = "A dataset downloaded from Uniprot containing human proteome information",
+                 dataframe_about = "This is the proteins dataset downloaded from Uniprot",
+                 dataframe_join_name = "proteins_join",
+                 dataframe_join_about = "This is a practice dataset for joining protiens",
+                 test = TRUE)
+
+prepare_yaml(title = "Tidybiology",
+             subtitle = "Intro to Data Science using R",
+             author = "Matthew Hirschey & Akshay Bareja",
+             date = as.Date("2020-11-09"),
+             test = TRUE)
+
+make_lecture(lecture_num = c(40,41,42,44,45,46,47),
+             name = "class3",
+             test = TRUE)
+
+launch_lecture(name = "class3",
+               test = TRUE)
+
+
 
 #class 4
+prepare_data(bespoke_dataframe = proteins,
+             test = TRUE)
+
+prepare_metadata(bespoke_dataframe = proteins,
+                 dataframe_name = "proteins",
+                 dataframe_title = "proteins",
+                 dataframe_source = "Uniprot",
+                 dataframe_desc = "A dataset downloaded from Uniprot containing human proteome information",
+                 dataframe_about = "This is the proteins dataset downloaded from Uniprot",
+                 dataframe_join_name = "proteins_join",
+                 dataframe_join_about = "This is a practice dataset for joining protiens",
+                 test = TRUE)
+
+prepare_yaml(title = "Tidybiology",
+             subtitle = "Intro to Data Science using R",
+             author = "Matthew Hirschey & Akshay Bareja",
+             date = as.Date("2020-11-11"),
+             test = TRUE)
+
+
+#stringr?
+make_lecture(lecture_num = c(50,51,52),
+             name = "class4",
+             test = TRUE)
+
+launch_lecture(name = "class4",
+               test = TRUE)
+
+
+
+#import, join, etc
+make_lecture(lecture_num = c(60,70),
+             name = "class4talk",
+             test = TRUE)
+
+launch_lecture(name = "class4talk",
+               test = TRUE)
+
 
 #class 5
 
+prepare_data(bespoke_dataframe = proteins,
+             test = TRUE)
+
+prepare_metadata(bespoke_dataframe = proteins,
+                 dataframe_name = "proteins",
+                 dataframe_title = "proteins",
+                 dataframe_source = "Uniprot",
+                 dataframe_desc = "A dataset downloaded from Uniprot containing human proteome information",
+                 dataframe_about = "This is the proteins dataset downloaded from Uniprot",
+                 dataframe_join_name = "proteins_join",
+                 dataframe_join_about = "This is a practice dataset for joining protiens",
+                 test = TRUE)
+
+prepare_yaml(title = "Tidybiology",
+             subtitle = "Intro to Data Science using R",
+             author = "Matthew Hirschey & Akshay Bareja",
+             date = as.Date("2020-11-13"),
+             test = TRUE)
+
+#rmd
+make_lecture(lecture_num = c(80,81,82),
+             name = "class5rmd",
+             test = TRUE)
+
+launch_lecture(name = "class5rmd",
+               test = TRUE)
 
 
+#conclusion
+make_lecture(lecture_num = c(90,91,92),
+             name = "class5conclu",
+             test = TRUE)
+
+launch_lecture(name = "class5conclu",
+               test = TRUE)
 
